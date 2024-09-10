31c5e606cc95147665096e661e57731c3cf5b0aa
Brammie15 <brammieverhulst@gmail.com>
2024-09-10 09:58:52 +0200

    add translate

--- custom.ts
+++ custom.ts
@@ -3,16 +3,20 @@ player.execute("/scriptevent cc:getId " + mobs.queryTarget((mobs.target(MY_AGENT
 
 enum CoralTypes {
     //% block="Red Coral"
+    //% block.loc.nl="Rood Koraal"
     Red,
     //% block="Yellow Coral"
+    //% block.loc.nl="Geel Kooraal"
     Yellow
 }
 
 enum LookDirection{
-    //% block="Boven"
+    //% block="Above"
+    //% block.loc.nl="boven"
     Above,
 
-    //% block="Onder"
+    //% block="Under"
+    //% block.loc.nl="Onder"
     Below,
 
     //% block="Voor"
@@ -21,20 +25,20 @@ enum LookDirection{
 
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
-    //% block="Zet klaar (level1)"
-    //% block.loc.en="Setup (level1)"
+    //% block="Zet klaar"
+    //% block.loc.en="Setup"
     export function setup_level1() {
         agent.teleport(world(2451, -2, 57), 0)
     }
 
-    //% block="Zet klaar (level2)"
-    //% block.loc.en="Setup (level2)"
+    //% block="Zet klaar"
+    //% block.loc.en="Setup"
     export function setup_level2(){
         agent.teleport(world(2446, -2, 40), EAST)
     }
 
-    //% block="Zet klaar (level3)"
-    //% block.loc.en="Setup (level3)"
+    //% block="Zet klaar"
+    //% block.loc.en="Setup"
     export function setup_level3() {
         agent.teleport(world(2447, -2, 30), NORTH)
     }
@@ -48,6 +52,7 @@ namespace CodeCosmos {
     //Slot 1: Blue Coral
 
     //% block="place $color below agent"
+    //% block.loc.nl="plaats $color onder de agent"
     export function place_coral(color?: CoralTypes) {
         // if(color == CoralTypes.Red){
         //     agent.setSlot(0);
@@ -74,6 +79,7 @@ namespace CodeCosmos {
 
     //% block="is $block $direction agent"
     //% block.shadow=minecraftBlock
+    //% block.loc.nl="is $block $direction agent"
     export function is_block_direction_agent(block: number, direction: LookDirection){
         let pos;
         switch(direction){
@@ -85,9 +91,9 @@ namespace CodeCosmos {
                 break;
         }
     }
-
-    //% block="is blok %block=block onder de agent"
-    //% block.loc.en="is block %block=block under the agent"
+    
+    //% block="is block %block=block under the agent"
+    //% block.loc.nl="is blok %block=block onder de agent"
     //% block.shadow=minecraftBlock
     export function is_block_under_agent(block: number): boolean {
         let block_under_agent = agent.getPosition().add(world(0,-1,0))
@@ -140,12 +146,14 @@ namespace CodeCosmos {
 //% color=#D83B01 weight=64 icon="icons/Agent_icon_white.png"
 namespace agent2 {
 
-    //% block="Ga vooruit"
+    //% block="Go forward"
+    //% block.loc.nl="Ga Vooruit"
     export function move_forward(){
         agent.move(FORWARD, 1);
     }
     
-    //% block="Draai om"
+    //% block="Turn"
+    //% block.loc.nl="Draai"
     export function turn_around() {
         agent.turn(LEFT)
         agent.turn(LEFT)
